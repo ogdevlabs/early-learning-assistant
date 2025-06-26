b6367aaf0d69d241bf3bb90e5175823094aeda63
Oscar Garcia <oscargarcia@ogdevlabs.onmicrosoft.com>
2025-06-26 16:45:32 -0500

    initial progress

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import logging
 from app_flow.system import FaceHandInteractionSystem
 from scoring.evaluator import ScoringEvaluator
+from video.capture import VideoCapture
+
 
 def main():
     logging.basicConfig(level=logging.INFO)
     scorer = ScoringEvaluator()
-    system = FaceHandInteractionSystem(scorer)
+    video = VideoCapture()
 
-    system.run()
+    video.run()
+
+    # system = FaceHandInteractionSystem(scorer)
+    #
+    # system.run()
 
 if __name__ == "__main__":
     main()
